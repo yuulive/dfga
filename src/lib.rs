b09a7851439e44854f110056215968723ed1bbef
Chris McComb <ccmcc2012@gmail.com>
2020-07-30 12:45:55 -0400

    Tests and reSTRUCTuring

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,123 @@
-/// This is the Sphere function, borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
+/// This is the Sphere function.
 ///
+/// The function is borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
 /// Although the function accepts a vector with an arbitrary number of inputs, this is what it looks like in 2D ![](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Sphere_function_in_3D.pdf/page1-800px-Sphere_function_in_3D.pdf.jpg)
-pub fn sphere(x: Vec<f64>) -> f64 {
-    let mut f = 0f64;
-    for i in 0..x.len() {
-        f -= x[i]*x[i];
+pub struct Sphere {}
+
+impl Sphere {
+    /// The bounds of the canonical sphere optimization problem are infinite.
+    pub const BOUNDS: (f64, f64) = (-f64::INFINITY, f64::INFINITY);
+
+    /// The global minimum is constant and zero
+    pub const MINIMUM: f64 = 0.0;
+
+    /// Function for evaluating
+    pub fn f(x: Vec<f64>) -> f64 {
+        let mut f = 0f64;
+        for i in 0..x.len() {
+            f -= x[i] * x[i];
+        }
+        f
     }
-    f
+
+    /// This function returns the minimizer (argument that will return the global minimum
+    pub fn minimizer(n: usize) -> Vec<f64> {
+        vec![0.0; n]
+    }
 }
 
-/// This is the Rastrigin function, borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
+/// This is the Rastrigin function.
 ///
+/// The function is borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
 /// Although the function accepts a vector with an arbitrary number of inputs, this is what it looks like in 2D ![](https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Rastrigin_function.png/800px-Rastrigin_function.png)
-pub fn rastrigin(x: Vec<f64>) -> f64 {
-    let a = 10.0;
-    let n = x.len() ;
-    let mut fx = a*(n as f64);
+pub struct Rastrigin {}
 
-    for i in 0..n {
-        fx += x[i].powi(2) - a*(2.0*x[i]*std::f64::consts::PI).cos();
+impl Rastrigin {
+    /// The bounds of the canonical sphere optimization problem are infinite.
+    pub const BOUNDS: (f64, f64) = (-5.12, 5.12);
+
+    /// The global minimum is constant and zero
+    pub const MINIMUM: f64 = 0.0;
+
+    /// Function for evaluating
+    pub fn f(x: Vec<f64>) -> f64 {
+        let a = 10.0;
+        let n = x.len() ;
+        let mut fx = a*(n as f64);
+
+        for i in 0..n {
+            fx += x[i].powi(2) - a*(2.0*x[i]*std::f64::consts::PI).cos();
+        }
+        fx
     }
-    fx
+
+    /// This function returns the minimizer (argument that will return the global minimum
+    pub fn minimizer(n: usize) -> Vec<f64> {
+        vec![0.0; n]
+    }
 }
 
-/// This is the Rosenbrock function, borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
+/// This is the Rosenbrock function.
 ///
+/// The function is borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
 /// Although the function accepts a vector with an arbitrary number of inputs, this is what it looks like in 2D ![](https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Rosenbrock%27s_function_in_3D.pdf/page1-800px-Rosenbrock%27s_function_in_3D.pdf.jpg)
-pub fn rosenbrock(x: Vec<f64>) -> f64 {
-    let n = x.len();
-    let mut fx = 0.0;
-    for i in 0..(n-1) {
-        fx += 100.0*(x[i+1] - x[i].powi(2)).powi(2) + (1.0 - x[i]).powi(2);
+pub struct Rosenbrock {}
+
+impl Rosenbrock {
+    /// The bounds of the canonical sphere optimization problem are infinite.
+    pub const BOUNDS: (f64, f64) = (-f64::INFINITY, f64::INFINITY);
+
+    /// The global minimum is constant and zero
+    pub const MINIMUM: f64 = 0.0;
+
+    /// Function for evaluating
+    pub fn f(x: Vec<f64>) -> f64 {
+        let n = x.len();
+        let mut fx = 0.0;
+        for i in 0..(n-1) {
+            fx += 100.0*(x[i+1] - x[i].powi(2)).powi(2) + (1.0 - x[i]).powi(2);
+        }
+        fx
     }
-    fx
+
+    /// This function returns the minimizer (argument that will return the global minimum
+    pub fn minimizer(n: usize) -> Vec<f64> {
+        vec![1.0; n]
+    }
 }
 
-/// This is the Ackley function, borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
+/// This is the Ackley function.
 ///
+/// The function is borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
 /// Although the function accepts a vector with an arbitrary number of inputs, this is what it looks like in 2D ![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Ackley%27s_function.pdf/page1-800px-Ackley%27s_function.pdf.jpg)
-pub fn ackley(x: Vec<f64>) -> f64 {
-    let n=x.len();
-    let mut fx = 0.0;
-    let mut square_sum = 0.0;
-    let mut cosine_sum = 0.0;
-    for i in 0..n {
-        square_sum += x[i].powi(2);
-        cosine_sum += (2.0*std::f64::consts::PI*x[i]).cos();
-    }
-    fx += -20.0*(-0.2*(0.5*square_sum).sqrt()).exp();
-    fx -= (0.5*cosine_sum).exp();
-    fx + std::f64::consts::E + 20.0
-}
+pub struct Ackley {}
 
-/// This is the Styblinski-Tang function, borrowed from [here](https://en.wikipedia.org/wiki/Test_functions_for_optimization).
-///
-/// Although the function accepts a vector with an arbitrary number of inputs, this is what it looks like in 2D ![](https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Styblinski-Tang_function.pdf/page1-800px-Styblinski-Tang_function.pdf.jpg)
-pub fn styblinski_tang(x: Vec<f64>) -> f64 {
-    let n = x.len();
-    let mut fx = 0.0;
-    for i in 0..n {
-        fx += x[i].powi(4) - 16.0*x[i].powi(2) + 5.0*x[i];
+impl Ackley {
+    /// The bounds of the canonical sphere optimization problem are infinite.
+    pub const BOUNDS: (f64, f64) = (-5.0, 5.0);
+
+    /// The global minimum is constant and zero
+    pub const MINIMUM: f64 = 0.0;
+
+    /// Function for evaluating
+    pub fn f(x: Vec<f64>) -> f64 {
+        let n=x.len();
+        let mut fx = 0.0;
+        let mut square_sum = 0.0;
+        let mut cosine_sum = 0.0;
+        for i in 0..n {
+            square_sum += x[i].powi(2);
+            cosine_sum += (2.0*std::f64::consts::PI*x[i]).cos();
+        }
+        fx += -20.0*(-0.2*(0.5*square_sum).sqrt()).exp();
+        fx -= (0.5*cosine_sum).exp();
+        fx + std::f64::consts::E + 20.0
     }
-    fx/2.0
+
+    /// This function returns the minimizer (argument that will return the global minimum
+    pub fn minimizer(n: usize) -> Vec<f64> {
+        vec![0.0; n]
+    }
 }
 
 #[cfg(test)]
@@ -70,16 +126,21 @@ mod two_d_tests {
 
     #[test]
     fn test_sphere() {
-        assert_eq!(sphere(vec![0.0; 2]), 0.0)
+        assert_eq!(Sphere::f(Sphere::minimizer(2)), Sphere::MINIMUM)
     }
 
     #[test]
     fn test_rastrigin() {
-        assert_eq!(rastrigin(vec![0.0; 2]), 0.0)
+        assert_eq!(Rastrigin::f(Rastrigin::minimizer(2)), Rastrigin::MINIMUM)
+    }
+
+    #[test]
+    fn test_rosenbrock() {
+        assert_eq!(Rosenbrock::f(Rosenbrock::minimizer(2)), Rosenbrock::MINIMUM)
     }
 
     #[test]
     fn test_ackley() {
-        assert_eq!(ackley(vec![0.0; 2]), 0.0)
+        assert_eq!(Ackley::f(Ackley::minimizer(2)), Ackley::MINIMUM)
     }
 }
